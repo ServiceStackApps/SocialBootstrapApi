63a69b78d57b3547cc1e9e423f3df276f2081736
Demis Bellot <demis.bellot@gmail.com>
2012-03-27 01:53:21 -0400

    remove unused Ext method

--- src/SocialBootstrapApi/App_Start/AppHost.cs
+++ src/SocialBootstrapApi/App_Start/AppHost.cs
@@ -168,12 +168,8 @@ namespace SocialBootstrapApi
 				() => new CustomUserSession(), //Use your own typed Custom UserSession type
 				new IAuthProvider[] {
                     new CredentialsAuthProvider(),         //HTML Form post of UserName/Password credentials
-                    new TwitterAuthProvider(appSettings) {
-						RedirectUrl = "http://bootstrapapi.apphb.com/friends"
-					},  //Sign-in with Twitter
-                    new FacebookAuthProvider(appSettings) {
-						RedirectUrl = "http://bootstrapapi.apphb.com/friends"
-					}, //Sign-in with Facebook
+                    new TwitterAuthProvider(appSettings),  //Sign-in with Twitter
+                    new FacebookAuthProvider(appSettings), //Sign-in with Facebook
                     new BasicAuthProvider(),               //Sign-in with Basic Auth
                 }));
 
@@ -211,27 +207,4 @@ namespace SocialBootstrapApi
 			//routes.IgnoreRoute("api/{*pathInfo}"); 
 		}
 	}
-
-	public static class AppHarborExtensions
-	{
-		public static string ToPublicUrl(this UrlHelper urlHelper, Uri relativeUri)
-		{
-			var httpContext = urlHelper.RequestContext.HttpContext;
-
-			var uriBuilder = new UriBuilder {
-				Host = httpContext.Request.Url.Host,
-				Path = "/",
-				Port = 80,
-				Scheme = "http",
-			};
-
-			if (httpContext.Request.IsLocal)
-			{
-				uriBuilder.Port = httpContext.Request.Url.Port;
-			}
-
-			return new Uri(uriBuilder.Uri, relativeUri).AbsoluteUri;
-		}
-	}
-
 }
