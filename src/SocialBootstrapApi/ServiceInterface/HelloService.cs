a94a94fb286fbf20ba10375568b82a0af5c06a7a
Demis Bellot <demis.bellot@gmail.com>
2012-10-14 02:48:57 -0400

    Added HelloService to SocialBootstrapApi

--- src/SocialBootstrapApi/ServiceInterface/HelloService.cs
+++ src/SocialBootstrapApi/ServiceInterface/HelloService.cs
@@ -1,26 +1,99 @@
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
+using ServiceStack.Text;
 
 namespace SocialBootstrapApi.ServiceInterface
 {
-	//Request DTO
-	public class Hello
-	{
-		public string Name { get; set; }
-	}
+    //Define Request and Response DTOs
+    [Route("/hellotext/{Name}")]
+    public class HelloText
+    {
+        public string Name { get; set; }
+    }
 
-	//Response DTO
-	public class HelloResponse
-	{
-		public string Result { get; set; }
-	}
+    [Route("/hellohtml/{Name}")]
+    public class HelloHtml
+    {
+        public string Name { get; set; }
+    }
 
-	//Can be called via any endpoint or format, see: http://servicestack.net/ServiceStack.Hello/
-	public class HelloService : Service
-	{
-		//Get's called by all HTTP Verbs (GET,POST,PUT,DELETE,etc) and endpoints JSON,XMl,JSV,etc
-	    public object Any(Hello request)
-		{
-			return new HelloResponse { Result = "Hello, Olle är en ÖL ål " + request.Name };
-		}
-	}
+    [Route("/helloimage/{Name}")]
+    public class HelloImage
+    {
+        public string Name { get; set; }
+
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public int? FontSize { get; set; }
+        public string Foreground { get; set; }
+        public string Background { get; set; }
+    }
+
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    //Implementation
+    public class HelloService : Service
+    {
+        public object Get(HelloHtml request)
+        {
+            return "<h1>Hello, {0}!</h1>".Fmt(request.Name);
+        }
+
+        [AddHeader(ContentType = "text/plain")]
+        public object Get(HelloText request)
+        {
+            return "<h1>Hello, {0}!</h1>".Fmt(request.Name);
+        }
+
+        [AddHeader(ContentType = "image/png")]
+        public object Get(HelloImage request)
+        {
+            var width = request.Width.GetValueOrDefault(640);
+            var height = request.Height.GetValueOrDefault(360);
+            var bgColor = request.Background != null ? Color.FromName(request.Background) : Color.ForestGreen;
+            var fgColor = request.Foreground != null ? Color.FromName(request.Foreground) : Color.White;
+
+            var image = new Bitmap(width, height);
+            using (var g = Graphics.FromImage(image))
+            {
+                g.Clear(bgColor);
+
+                var drawString = "Hello, {0}!".Fmt(request.Name);
+                var drawFont = new Font("Times", request.FontSize.GetValueOrDefault(40));
+                var drawBrush = new SolidBrush(fgColor);
+                var drawRect = new RectangleF(0, 0, width, height);
+
+                var drawFormat = new StringFormat {
+                    LineAlignment = StringAlignment.Center,
+                    Alignment = StringAlignment.Center
+                };
+
+                g.DrawString(drawString, drawFont, drawBrush, drawRect, drawFormat);
+
+                var ms = new MemoryStream();
+                image.Save(ms, ImageFormat.Png);
+                return ms;
+            }
+        }
+
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, {0}!".Fmt(request.Name) };
+
+            //C# client can call with:
+            //var response = client.Get(new Hello { Name = "ServiceStack" });
+        }
+    }
 }
