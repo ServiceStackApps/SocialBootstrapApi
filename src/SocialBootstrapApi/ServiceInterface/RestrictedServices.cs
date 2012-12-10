b00067d7211d6a378e9469c1df1d41ad1fbad720
Demis Bellot <demis.bellot@gmail.com>
2012-12-10 04:10:31 -0500

    Added RestrictedServices to showcase new Access Restriction featres

--- src/SocialBootstrapApi/ServiceInterface/RestrictedServices.cs
+++ src/SocialBootstrapApi/ServiceInterface/RestrictedServices.cs
@@ -1,10 +1,83 @@
-﻿namespace SocialBootstrapApi.ServiceInterface
-{
-    //[Restrict()]
-    //public class JsonOnly
-    //{
-    //     pu
-    //}
+﻿using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
 
-    
+namespace SocialBootstrapApi.ServiceInterface
+{
+    [Restrict(EndpointAttributes.Json)]
+    public class JsonOnly { }
+
+    [Restrict(EndpointAttributes.Xml)]
+    public class XmlOnly { }
+
+    [Restrict(VisibilityTo = EndpointAttributes.Json)]
+    public class VisibleToJsonOnly { }
+
+    [Restrict(VisibilityTo = EndpointAttributes.Xml)]
+    public class VisibleToXmlOnly { }
+
+    [Restrict(EndpointAttributes.Localhost)]
+    public class LocalhostOnly { }
+
+    [Restrict(EndpointAttributes.InternalNetworkAccess)]
+    public class InternalOnly { }
+
+    [Restrict(EndpointAttributes.External)]
+    public class ExternalOnly { }
+
+    public class GetOnly { }
+    public class PostOnly { }
+
+    public class Response
+    {
+        public string Result { get; set; }
+    }
+
+    public class RestrictedServices : Service
+    {
+        public Response Any(JsonOnly request)
+        {
+            return new Response { Result = "Haz Sekritz!" };
+        }
+
+        public Response Any(XmlOnly request)
+        {
+            return new Response { Result = "Haz Sekritz!" };
+        }
+
+        public Response Any(VisibleToJsonOnly request)
+        {
+            return new Response { Result = "Haz Sekritz!" };
+        }
+
+        public Response Any(VisibleToXmlOnly request)
+        {
+            return new Response { Result = "Haz Sekritz!" };
+        }
+
+        public Response Any(LocalhostOnly request)
+        {
+            return new Response { Result = "Haz Sekritz!" };
+        }
+
+        public Response Any(InternalOnly request)
+        {
+            return new Response { Result = "Haz Sekritz!" };
+        }
+
+        public Response Any(ExternalOnly request)
+        {
+            return new Response { Result = "Haz Sekritz!" };
+        }
+
+        public Response Any(GetOnly request)
+        {
+            return new Response { Result = "Haz Sekritz!" };
+        }
+
+        public Response Any(PostOnly request)
+        {
+            return new Response { Result = "Haz Sekritz!" };
+        }
+
+    }
 }
