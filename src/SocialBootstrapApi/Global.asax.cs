88d5014a3163364a2ab36bf6dd2bd83f67bec538
Demis Bellot <demis.bellot@gmail.com>
2014-03-19 17:40:35 -0400

    reformat AppHost

--- src/SocialBootstrapApi/Global.asax.cs
+++ src/SocialBootstrapApi/Global.asax.cs
@@ -9,32 +9,32 @@ using ServiceStack.MiniProfiler;
 
 namespace SocialBootstrapApi
 {
-	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	// visit http://go.microsoft.com/?LinkId=9394801
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
 
-	public class MvcApplication : System.Web.HttpApplication
-	{
-		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-		{
-			filters.Add(new HandleErrorAttribute());
-		}
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
 
-		public static void RegisterRoutes(RouteCollection routes)
-		{
-			routes.IgnoreRoute("Content/{*pathInfo}");
-			routes.IgnoreRoute("api/{*pathInfo}"); 
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("Content/{*pathInfo}");
+            routes.IgnoreRoute("api/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			routes.MapRoute(
-				"Default", // Route name
-				"{controller}/{action}/{id}", // URL with parameters
-				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-			);
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
 
-			routes.MapRoute("CatchAll", "{*url}",
-				new { controller = "Home", action = "Index" }
-			);
-		}
+            routes.MapRoute("CatchAll", "{*url}",
+                new { controller = "Home", action = "Index" }
+            );
+        }
 
         protected void Application_Start()
         {
@@ -51,18 +51,18 @@ namespace SocialBootstrapApi
             new AppHost().Init();
         }
 
-		protected void Application_BeginRequest(object sender, EventArgs e)
-		{
-			Console.WriteLine("Application_BeginRequest");
-			
-			if (Request.IsLocal)
-				Profiler.Start();
-		}
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Console.WriteLine("Application_BeginRequest");
 
-		protected void Application_EndRequest(object sender, EventArgs e)
-		{
-			Console.WriteLine("Application_EndRequest");
-			Profiler.Stop();
-		}
-	}
+            if (Request.IsLocal)
+                Profiler.Start();
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            Console.WriteLine("Application_EndRequest");
+            Profiler.Stop();
+        }
+    }
 }
